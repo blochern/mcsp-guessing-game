34189ebffb18f32e3d516074e161a95e4fdb82d3
Nathanael Blocher <blocher.nathanael@gmail.com>
2023-12-12 09:22:34 -0800

    Feature 7 completed

--- app.js
+++ app.js
@@ -1,34 +1,75 @@
-const randomNumber = (max) => Math.ceil(Math.random() * max);
+// function for a random number
+function randomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
 
-const game = (user) => {
-    let max = randomNumber(50);
+let max = 50;
 
-    let secret = randomNumber(max);
+// this global variable will store game results for users
+let memory = {};
 
+// the main function
+function game(user) {
+    // generate a random number between 1-50
+    let secret = randomNumber(50);
+
+    // declare our input variable (but leave it undefined for now)
     let input;
 
-    let memory = {}; memory.user = [];
+    // variable for previous game data (if applicable)
+    let previousGame;
 
-    // game start/incorrect answer
+    // if this user already has saved data in memory, grab it and cache it for later
+    if (memory[user]) {
+        previousGame = memory[user];
+    }
+
+    // make space to store the user's inputs
+    memory[user] = [];
+
+    // while input is not equal to secret (e.g. incorrect guess or game start)
     while (input !== secret) {
-        input = +prompt(`Please enter a number between 1 and ${max}`);
+        // prompt the user for a number and parse it immediately
+        input = parseInt(prompt(`Please enter a number between 1 and ${max}`));
 
-        // bad input/out of bounds inputs aren't added to the guess count
+        // a failed parseInt will return NaN, which is falsy | 0 is also falsy
+        // if the input is falsy (invalid), or out of bounds (higher than max, lower than 1)
         if (!input || input > max || input < 1) {
+            // invalid guesses aren't added to memory
             alert("Invalid guess!"); continue;
         }
 
-        // too high/too low
-        if (input < secret) { alert(`Sorry, ${user}, guess higher!`); memory.user.push(input); }
-        if (input > secret) { alert(`Incorrect, ${user}, guess lower!`); memory.user.push(input); }
+        // too high/too low? alert the user, and push the input to memory
+        if (input < secret) {
+            alert(`Sorry, ${user}, guess higher!`);
+        }
+        if (input > secret) {
+            alert(`Incorrect, ${user}, guess lower!`);
+        }
+        memory[user].push(input);
     }
-    alert(`Correct guess! Your previous guesses were ${memory.user}.`);
-    // ask to play again
-    if (confirm(`Do you want to play again, ${user}?`)) {
-        game(prompt("Please enter your name, user!"));
+    // the while loop only stops if the input === the secret number
+    alert(`Correct guess! Your previous guesses were ${memory[user]}.`);
+
+    // if we have a previous game stored, compare this game to the previous game
+    if (previousGame) {
+        if (previousGame.length < memory[user].length) {
+            alert(`You did worse than your previous game, ${user}, you took ${memory[user].length} guesses compared to ${previousGame.length} guesses.`);
+        } else if (previousGame.length > memory[user].length) {
+            alert(`You did better than your previous game, ${user}, you took ${memory[user].length} guesses compared to ${previousGame.length} guesses.`);
+        } else {
+            alert(`You tied your previous game, ${user}, with ${memory[user].length} guesses.`);
+        }
     }
-    else { alert(`Thanks for playing, ${user}!`); }
+    console.log(memory);
 }
 
-// initialize
+// game start
 game(prompt("Please enter your name, user!"));
+
+// this while loop evaluates to true if the user keeps saying they want to play again
+while (confirm(`Do you want to play again?`)) {
+    game(prompt("Please enter your name, user!"));
+}
+
+alert(`Thanks for playing!`);
