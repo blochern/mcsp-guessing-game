f89563add93e364ae171a91b5c0b2507eadb4d5c
Nathanael Blocher <blocher.nathanael@gmail.com>
2023-12-12 07:53:17 -0800

    Feature 6 complete

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
-function randomNumber(max) {
-    return Math.ceil(Math.random() * max);
-}
+const randomNumber = (max) => Math.ceil(Math.random() * max);
 
-let max = randomNumber(50);
+const game = (user) => {
+    let max = randomNumber(50);
 
-let secret = randomNumber(max);
+    let secret = randomNumber(max);
 
-let input;
+    let input;
 
-while (input !== secret) {
-    input = +prompt(`Please enter a number between 1 and ${max}`);
+    let memory = {}; memory.user = [];
 
-    if (!input || input > max || input < 1) {
-        alert("Invalid guess!"); continue;
+    // game start/incorrect answer
+    while (input !== secret) {
+        input = +prompt(`Please enter a number between 1 and ${max}`);
+
+        // bad input/out of bounds inputs aren't added to the guess count
+        if (!input || input > max || input < 1) {
+            alert("Invalid guess!"); continue;
+        }
+
+        // too high/too low
+        if (input < secret) { alert(`Sorry, ${user}, guess higher!`); memory.user.push(input); }
+        if (input > secret) { alert(`Incorrect, ${user}, guess lower!`); memory.user.push(input); }
     }
-
-    if (input < secret) { alert("Guess higher!"); }
-    if (input > secret) { alert("Guess lower!"); }
+    alert(`Correct guess! Your previous guesses were ${memory.user}.`);
+    // ask to play again
+    if (confirm(`Do you want to play again, ${user}?`)) {
+        game(prompt("Please enter your name, user!"));
+    }
+    else { alert(`Thanks for playing, ${user}!`); }
 }
 
-alert("Correct!");
+// initialize
+game(prompt("Please enter your name, user!"));
